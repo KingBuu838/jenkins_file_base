4391e722b015a588d220f864b63f1d094c87d158
KingBuu838 <229227597+KingBuu838@users.noreply.github.com>
2026-03-28 15:17:27 -0400

    commit fourth

--- 3-bucket.tf
+++ 3-bucket.tf
@@ -42,3 +42,29 @@ resource "aws_s3_object" "armageddon_links" {
 
   etag = filemd5("./proof/armageddon_links.md")
 }
+
+resource "aws_s3_bucket_public_access_block" "class7-gcheck" {
+  bucket = aws_s3_bucket.class7-gcheck.id
+
+  block_public_acls  = true
+  ignore_public_acls = true
+
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "gcheck" {
+  bucket = aws_s3_bucket.class7-gcheck.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicAccess"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.class7-gcheck.arn}/*"
+      }
+    ]
+  })
+}
